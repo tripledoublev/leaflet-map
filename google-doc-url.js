09af32ffb796985d695968acee636b1b29e85fce
vincent <vincentcharlebois@gmail.com>
2022-12-12 17:59:36 -0500

    updated credential

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1S0zASACnaZ40NF8kTkDwn0lRAfx3NmQ6pR8ujeBC2V8/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
+var googleApiKey = 'AIzaSyDwJDHDk6cHzoKah6K03knezASc3olIr0g';
